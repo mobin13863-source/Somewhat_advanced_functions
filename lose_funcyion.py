ecfe10edd10d9c414ae412c339fae440c6ef4348
Mobin <234991811+mobin13863-source@users.noreply.github.com>
2025-11-13 18:16:53 +0330

    Create lose_funcyion.py
    the functin can gives name like (mobin) and ,
            it turns into to big words like   mobin after MOBIN

---
+++ lose_funcyion.py
@@ -0,0 +1,8 @@
+def kochek_kardn(name):
+    ''' the functin can gives name like (mobin) and ,
+        it turns into to big words like   mobin after MOBIN
+    '''
+    x=name.upper()
+    print(f"the {name}, the after{x}")
+    
+kochek_kardn("mobin")
